68831c28ecbe8c795532ffea688c877a4d2d9ef9
Ivan Ponomarev <iponomarev@avito.ru>
2016-03-22 16:43:55 +0300

    use subjects instead of booleans

--- example/src/main/kotlin/com/ipvans/rm/example/screen/MainModel.kt
+++ example/src/main/kotlin/com/ipvans/rm/example/screen/MainModel.kt
@@ -3,6 +3,7 @@ package com.ipvans.rm.example.screen
 import android.util.Log
 import com.ipvans.rm.retainedmodel.Model
 import rx.Observable
+import rx.lang.kotlin.BehaviorSubject
 import rx.lang.kotlin.PublishSubject
 import rx.lang.kotlin.plusAssign
 import rx.subscriptions.CompositeSubscription
@@ -10,10 +11,10 @@ import java.util.concurrent.TimeUnit
 
 class MainModel() : Model<MainPresenter>() {
 
-    //todo consider using Observable
-    private var containerDestroyed = false
-    private var countdownFinished = false
-    //
+    private val countDownFinishSubject = BehaviorSubject<Boolean>()
+    private val containerDestroySubject = BehaviorSubject<Boolean>()
+    private val forceDestroySubject = BehaviorSubject<Boolean>()
+
     private val modelLifecycleSubscriptions = CompositeSubscription()
     private val viewLifecycleSubscriptions = CompositeSubscription()
 
@@ -21,7 +22,7 @@ class MainModel() : Model<MainPresenter>() {
 
     val countdown = clicks
             .switchMap {
-                countdownFinished = false
+                countDownFinishSubject.onNext(false)
                 Observable.from(20 downTo 0)
                         .zipWith(Observable.interval(0, 1, TimeUnit.SECONDS)) {
                             count, i ->
@@ -31,19 +32,26 @@ class MainModel() : Model<MainPresenter>() {
             }
             .doOnNext {
                 if (it == 0) {
-                    Log.i("MODEL", "countdown finished, container destroyed = $containerDestroyed")
-                    countdownFinished = true
-                    // if activity is destroyed and our long task is finished
-                    // destroy model
-                    if (containerDestroyed) finish()
+                    Log.i("MODEL", "countdown finished, container destroyed = ${containerDestroySubject.value}")
+                    countDownFinishSubject.onNext(true)
                 }
             }
             .publish()
 
+    private val destroyEventObservable = Observable.combineLatest(
+            countDownFinishSubject, containerDestroySubject, forceDestroySubject.startWith(false)) {
+        countdown, container, forceDestroy ->
+        (countdown && container) || forceDestroy
+    }
+            .filter { it }
+            .doOnNext { finish() }
+            .publish()
+
     override fun onCreate() {
         Log.i("MODEL", "create model")
-        containerDestroyed = false
+        containerDestroySubject.onNext(false)
         modelLifecycleSubscriptions += countdown.connect()
+        modelLifecycleSubscriptions += destroyEventObservable.connect()
     }
 
     override fun attachPresenter(presenter: MainPresenter) {
@@ -58,8 +66,7 @@ class MainModel() : Model<MainPresenter>() {
 
     override fun onDestroy() {
         Log.i("MODEL", "destroy model")
-        containerDestroyed = true
-        if (countdownFinished) finish()
+        containerDestroySubject.onNext(true)
     }
 
     fun finish() {
